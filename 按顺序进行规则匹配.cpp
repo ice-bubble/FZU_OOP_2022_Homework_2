b14929384134318cd2d5a6528aa23bc246738002
ice-bubble <ice_snow@ice-bubble.com>
2022-01-27 17:12:12 +0800

    修复了规则匹配模块的已知bug，并成功输出dataset1中packet1的正确答案

--- 按顺序进行规则匹配.cpp
+++ 按顺序进行规则匹配.cpp
@@ -5,7 +5,7 @@ int rule_match(unsigned int ip0, unsigned int ip1, int d0, int d1, int x, unsign
 	if (ip1 >= ip1min && ip1 <= ip1max)c++;
 	if (d0 >= d01 && d0 <= d02)c++;
 	if (d1 >= d11 && d1 <= d12)c++;
-	if (x >= x0 && x <= x1)c++;
+	if (x == x0 || x1 == 0)c++;
 	if (c == 5)return 1;
 	else return 0;
 }
