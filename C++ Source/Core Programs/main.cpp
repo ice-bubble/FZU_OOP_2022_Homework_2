9bdbfee8843e81fe823cd5c8309fe750dd5853f4
ice-bubble <ice_snow@ice-bubble.com>
2022-01-30 00:32:36 +0800

    main.cpp文件中增加了部分注释

--- C++ Source/Core Programs/main.cpp
+++ C++ Source/Core Programs/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 class rule
 {
 public:
-	int in(ifstream& fp_rule);
+	int in(ifstream& fp_rule);//fstream的拷贝（赋值）构造函数是已删除函数，“&”表示是将函数的形参设置为引用类型，防止报错
 	class rule* create_chain_table(ifstream& fp_rule);
 	void visit_chain_table(rule* p);
 	void visit_processed_chain_table(rule* p, ifstream& fp_packet, ofstream& fp_out);
